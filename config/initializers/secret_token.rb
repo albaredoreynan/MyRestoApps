21e55b934d5fa8de064f6b4c412068b8383bd598
Reynan Perez Albaredo <chezca1228@yahoo.com>
2012-08-30 15:06:53 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RestoApps::Application.config.secret_token = '0a3a60c7efb5eff4b100d3bbee918c6b88556a1443e9b960060acfba48e2ab45f8d71956ad94806f8bdc8a67dcad20860d22ac873d8576871aae13a5dc2b8a28'
